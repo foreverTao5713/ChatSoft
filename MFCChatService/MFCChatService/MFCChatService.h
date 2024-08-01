34de40565758ee8664a45bba2ac86c149f4ad16a
zhangtao5713 <zt275@DESKTOP-O9FB481>
2024-08-01 09:41:44 +0800

    上传原始代码

---
+++ MFCChatService/MFCChatService/MFCChatService.h
@@ -0,0 +1,32 @@
+﻿
+// MFCChatService.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFCChatServiceApp:
+// 有关此类的实现，请参阅 MFCChatService.cpp
+//
+
+class CMFCChatServiceApp : public CWinApp
+{
+public:
+	CMFCChatServiceApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCChatServiceApp theApp;
---
+++ mfc_chatDlg_project/mfc_chatDlg_project/mfc_chatDlg_projectDlg.h
@@ -0,0 +1,37 @@
+﻿
+// mfc_chatDlg_projectDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CmfcchatDlgprojectDlg 对话框
+class CmfcchatDlgprojectDlg : public CDialogEx
+{
+// 构造
+public:
+	CmfcchatDlgprojectDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC_CHATDLG_PROJECT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedConnectBtn();
+	afx_msg void OnBnClickedDisconnectBtn();
+};
